bb6a8fc3ce97af7c09035727306e0d7337bee813
okwhy <59335640+okwhy@users.noreply.github.com>
2024-03-31 14:57:18 +0400

    type mismatch handling

--- src/main/java/ru/farafonov/responses/errorhandling/ErrorsMsg.java
+++ src/main/java/ru/farafonov/responses/errorhandling/ErrorsMsg.java
@@ -1,9 +1,12 @@
 package ru.farafonov.responses.errorhandling;
 
-public class ErrorsMsg {
+public final class ErrorsMsg {
     private ErrorsMsg(){
 
     }
     public static final String NOT_VALID_SALARY = "Средняя зарплата не должна быть меньше нуля";
     public static final String NOT_VALID_AMOUNT = "Количество дней отпуска не должно быть меньше нуля";
+    public static final String WRONG_FORMAT_DATE ="Дата выхода в отпуск введена в неверном формате";
+    public static final String WRONG_FORMAT_SALARY ="Зарплата введена в неверном формате";
+    public static final String WRONG_FORMAT_DAYS ="Количество дней отпуска введено в неверном формате";
 }
--- src/main/java/ru/farafonov/responses/errorhandling/ValExceptionHandler.java
+++ src/main/java/ru/farafonov/responses/errorhandling/ValExceptionHandler.java
@@ -2,10 +2,13 @@ package ru.farafonov.responses.errorhandling;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ru.farafonov.responses.APIResponse;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Set;
 
 @RestControllerAdvice
@@ -18,5 +21,17 @@ public class ValExceptionHandler {
             msg.append(cv.getMessage()).append(" | ");
         }
         return APIResponse.badRequest(null,"ER-001","Violation of constraints", msg.toString());
+    }
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public APIResponse<String> handleTypeMismatch(MethodArgumentTypeMismatchException e){
+        Class reqtype=e.getRequiredType();
+        if (reqtype== LocalDate.class) return APIResponse.
+                badRequest(null,"ER-002","Type Mismatch",ErrorsMsg.WRONG_FORMAT_DATE);
+        if (reqtype== BigDecimal.class) return APIResponse.
+                badRequest(null,"ER-002","Type Mismatch",ErrorsMsg.WRONG_FORMAT_SALARY);
+        if (reqtype== int.class) return APIResponse.
+                badRequest(null,"ER-002","Type Mismatch",ErrorsMsg.WRONG_FORMAT_DAYS);
+        return APIResponse.
+                badRequest(null,"ER-002","Type Mismatch",null);
     }
 }
